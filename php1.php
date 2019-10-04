668cabcc418d14562ab22fb64c87bd341fe3b10c
Astidwi <53321153+Astidwi@users.noreply.github.com>
2019-10-04 09:30:20 +0700

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Hello world-!";
+    echo "<h1>Hello world<h1>-!";
+
+    //VARIABEL
+    //diawali dengan $
+    //nama variabel tidak boleh dawali dengan angka
+    //tidak boleh mengandung angka
+    $nama = "Nama kalian";
+    $pesan = "Hallo selamat datang<strong>$nama</strong>";
+
+    echo $pesan;
+
+?>
